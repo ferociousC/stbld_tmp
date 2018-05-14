fc472933f57e896cae91bf83218f3309e642a506
magicfrog <abel.a.bodo@gmail.com>
2018-05-14 17:35:29 +0200

    Initial commit

---
+++ src/app/index.js
@@ -0,0 +1,17 @@
+var ng = require('@angular/core');
+var ngPlatformBrowser = require('@angular/platform-browser');
+
+var HelloComponent = require('./hello');
+
+module.exports = ng.NgModule({
+  imports: [
+    ngPlatformBrowser.BrowserModule,
+  ],
+  declarations: [
+    HelloComponent
+  ],
+  bootstrap: [HelloComponent]
+})
+.Class({
+  constructor: function () {}
+});
